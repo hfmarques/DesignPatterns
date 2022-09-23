332b5e4773f356d015c30b06eb58109a4f634e69
Héber Marques <heber.f.marques@gmail.com>
2022-09-23 09:50:41 -0300

    Added new types of builders

---
+++ FunctionalBuilder/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var pb = new PersonBuilder();
+var person = pb.Called("Dmitri").WorksAsA("Programmer").Build();
+
+public class Person
+{
+    public string Name, Position;
+}
+
+public sealed class PersonBuilder
+{
+    public readonly List<Action<Person>> Actions
+        = new List<Action<Person>>();
+
+    public PersonBuilder Called(string name)
+    {
+        Actions.Add(p => { p.Name = name; });
+        return this;
+    }
+
+    public Person Build()
+    {
+        var p = new Person();
+        Actions.ForEach(a => a(p));
+        return p;
+    }
+}
+
+public static class PersonBuilderExtensions
+{
+    public static PersonBuilder WorksAsA
+        (this PersonBuilder builder, string position)
+    {
+        builder.Actions.Add(p => { p.Position = position; });
+        return builder;
+    }
+}
---
+++ StepwiseBuilder/Program.cs
@@ -0,0 +1,74 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var car = CarBuilder.Create()
+    .OfType(CarType.Crossover)
+    .WithWheels(18)
+    .Build();
+Console.WriteLine(car);
+
+public enum CarType
+{
+    Sedan,
+    Crossover
+};
+
+public class Car
+{
+    public CarType Type;
+    public int WheelSize;
+}
+
+public interface ISpecifyCarType
+{
+    public ISpecifyWheelSize OfType(CarType type);
+}
+
+public interface ISpecifyWheelSize
+{
+    public IBuildCar WithWheels(int size);
+}
+
+public interface IBuildCar
+{
+    public Car Build();
+}
+
+public class CarBuilder
+{
+    public static ISpecifyCarType Create()
+    {
+        return new Impl();
+    }
+
+    private class Impl :
+        ISpecifyCarType,
+        ISpecifyWheelSize,
+        IBuildCar
+    {
+        private Car car = new();
+
+        public ISpecifyWheelSize OfType(CarType type)
+        {
+            car.Type = type;
+            return this;
+        }
+
+        public IBuildCar WithWheels(int size)
+        {
+            switch (car.Type)
+            {
+                case CarType.Crossover when size < 17 || size > 20:
+                case CarType.Sedan when size < 15 || size > 17:
+                    throw new ArgumentException($"Wrong size of wheel for {car.Type}.");
+            }
+
+            car.WheelSize = size;
+            return this;
+        }
+
+        public Car Build()
+        {
+            return car;
+        }
+    }
+}
