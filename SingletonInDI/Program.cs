ed7e98f3ccca4634d414c8c3853de10e10396d3b
Héber Marques <heber.f.marques@gmail.com>
2022-10-03 10:14:54 -0300

    Singleton

---
+++ SingletonInDI/Program.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+
+var builder = new ContainerBuilder();
+builder.RegisterType<EventBroker>().SingleInstance();
+builder.RegisterType<Foo>();
+
+using (var c = builder.Build())
+{
+    var foo1 = c.Resolve<Foo>();
+    var foo2 = c.Resolve<Foo>();
+        
+    Console.WriteLine(ReferenceEquals(foo1, foo2));
+    Console.WriteLine(ReferenceEquals(foo1.Broker, foo2.Broker));
+}
+
+public class Foo
+{
+    public EventBroker Broker;
+
+    public Foo(EventBroker broker)
+    {
+        Broker = broker ?? throw new ArgumentNullException(paramName: nameof(broker));
+    }
+}
+
+public class EventBroker
+{
+    
+}
